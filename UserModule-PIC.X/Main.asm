beb5d41b0aaf487afde172d65b9aabfb162e251f
Yves <raphael.bastos@me.com>
2016-10-26 12:45:25 -0300

    Adaptations to Main.asm
    Simplified version.
    1) No timers (for now)
    2) Button treatment separation
    3) Ports setup corrected

--- UserModule-PIC.X/Main.asm
+++ UserModule-PIC.X/Main.asm
@@ -1,6 +1,9 @@
-;TODO list
+;;;;;;;;;;; TODO list ;;;;;;;;;;;
 ; 1. check if config header is correct
-; 2. change buttons to pins RA
+; 2. which registers will we use to save the queue?
+; 3. add goSleep functionality (to save energy) and wakeUp when button is pressed
+; 4. do we need a reset button?
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     
 ; PIC16F628A Configuration Bit Settings
 ; ASM source line config statements
@@ -11,78 +14,54 @@
  
 	ORG	0x0000
 iCount	EQU	d'241'
-goSleep	EQU	0x0
 	
 	GOTO	setup				    
-	ORG	0x0004		    ;TRATAMENTO DAS ROTINAS DE INTERRUPÇÃO (em todos os PICs, localiza-se no endereço 0x0004)
-	
-	BTFSC	INTCON, RBIF
-	call	buttonInterrupt
-	BTFSC	INTCON, T0IF
-	call	timerOut
+	ORG	0x0004		    ; Interruption treatment 	
+	BTFSC	INTCON, RAIF	   
+	call	buttonInterrupt	    ;PORTA interrupt flag bit set
+	RETFIE			    ;Return from interrupt treatment 	    
 
-	
-	RETFIE			    ;Retorna da rotina de interrupção	    
-
-timerOut:
-	INCF	PORTB	
-	BCF	INTCON, T0IF	    ;Zerando o flag de overflow do timer 
-	BCF	INTCON, T0IE	    ;Desabilitando overflow interrupt do timer (por que??)
-	BSF	goSleep,0	    ;Setar bit dormir para entrar no sleep do loop principal
-	RETURN
 	
 buttonInterrupt:
-				    ;CHECAR QUAL BOTÃO FOI APERTADO
-	BTFSS	PORTB, RB6	    ; Quando for o RB6
-	call	buttonrb6
-	BTFSS	PORTB, RB7	    ; Quando for o RB7
-	call	buttonrb7
-	BCF	goSleep,0	    ;Limpar bit dormir para não dormir no loop central
+				    ;Checks which button was pressed 
+	BTFSS	PORTA, RA0	     
+	call	buttonra0	    ;RA0 pressed
+	BTFSS	PORTA, RA1	    
+	call	buttonra1	    ;RA1 pressed
+	BTFSS	PORTA, RA2	    
+    	call	buttonra2	    ;RA2 pressed
+	BTFSS	PORTA, RB3	
+	call	buttonra3	    ;RA3 pressed
 
-buttonrb6:
-	BTFSC	INTCON, T0IE	    ;Se interrupt timer tiver desativada, continuar nesta funcao
+buttonra0:
+	;TODO
+	RETURN	
+buttonra1:
+	;TODO
 	RETURN
-	MOVLW	iCount		    ;Move iCount para W
-	MOVWF	TMR0		    ;Pega o iCount do W para setar como tempo inicial do timer
-	BSF	INTCON, T0IE
+buttonra2:
+	;TODO
 	RETURN
-	
-buttonrb7:
-	MOVLW	0x00
-	MOVWF	PORTB		    ;resetar portb
+buttonra3:
+	;TODO
 	RETURN
 	
 setup:
-	BANKSEL	TRISA		    ;Inicializando PortA
-	MOVLW	0x00		    
-	MOVWF	TRISA		    ;Seta todos os pins para saída
+	;configure ports
+	BANKSEL	PORTA
+	CLRF	PORTA
+	MOVLW	'00000000'	    ;all pins set to digital I/O...
+	MOVWF	CMCON0		    ;so there will be no conflict
+	BANKSEL	ANSEL		
+	CLRF	ANSEL		    
+	MOVLW	'00111111'	    ;RA<5:0> as inputs
 	
-	BANKSEL PORTA
-	CLRF	PORTA		    ; RA6 e RA7 podem operar como saída, caso o oscilador esteja no modo _FOSC_INTOSCIO
-
-	MOVLW	0x07
-	MOVWF	CMCON		    ; Desabilitando Comparadores para não conflitar com pins RB
 	
-	BANKSEL	PORTB	
-	MOVLW	b'11000000'	    ; RA7 e RA6 como entrada, demais como saída
-	MOVWF	TRISB
-		
-	BANKSEL	OPTION_REG	    ;CONFIGURACOES OPTION REG
-	MOVLW	b'00010011'	    ;Configurando pull-up, rising edge, clock interno, high-to-low, pre-escalar 1:16
-	MOVWF	OPTION_REG	    ;Seta
-	
-	BANKSEL	PCON
-	BCF	PCON, OSCF	    ;Configura oscilador para 48KHz
-	
-	BANKSEL	PORTA		   ;CONFIGURAR INTERRUPCOES
-	MOVLW	b'10001000'	    ;habilitar interrupcoes globais, timer e de botão; desabilita todo o resto
-	MOVWF	INTCON		    ;Seta
+	BANKSEL	PORTA		    ;Interruption setup
+	MOVLW	b'11001000'	    ;enable global interruptions, pheriperals and PORTA
+	MOVWF	INTCON		    
 	
 loop:	
-	BTFSC	goSleep, 0
-	SLEEP
-	
+	NOP
 	GOTO	loop	
-	
-
 	END
