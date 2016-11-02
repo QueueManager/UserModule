52a9ed5249d0d220249d4eb5f5a7cd0c8b4a1671
limadmarlon <limadmarlon@gmail.com>
2016-11-01 21:56:34 -0300

    All Buttons are working
    1. All buttons execute their functions correctly
    2. All Normal Queues have 30 positions
    3. All Priority Queues have 12 positions
    4. the addresses of all queues will be documented in our wiki page

--- UserModule-PIC.X/Main.asm
+++ UserModule-PIC.X/Main.asm
@@ -1,11 +1,12 @@
 ;;;;;;;;;;; TODO list ;;;;;;;;;;;
 ; 1. check if config header is correct
-; 2. which registers will we use to save the queue?
+; 2. DONE
 ; 3. add goSleep functionality (to save energy) and wakeUp when button is pressed
 ; 4. do we need a reset button?
 ; 5. external communications:
 ;   5.1 configure output when button is pressed (printer, etc)
 ;   5.2 create function to receive request for next in queue from other modules
+; 6. Testar o finalzinho de todas as filas verificando se nao estar invadindo as outras
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     
 ; PIC16F883 Configuration Bit Settings
@@ -20,8 +21,8 @@ __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_ON & _PWRTE_OFF & _MCLRE_ON & _C
  
 	ORG	0x0000
 iCount	EQU	d'241'
-;mQueue	EQU	h'40'		    ;manager queue register
-;cQueue	EQU	h'50'		    ;cashier queue register
+mQueue	EQU	h'40'		    ;manager queue register
+cQueue	EQU	h'50'		    ;cashier queue register
 	
 	GOTO	setup				    
 	ORG	0x0004		    ;Interruption treatment 	
@@ -36,45 +37,95 @@ iCount	EQU	d'241'
 	    endc
 clearMqueue:
 	MOVLW	0x00
-	MOVWF	mBegin
+	MOVWF	mCount
 	MOVLW	0x2A
 	MOVWF	mBegin
+	RETURN	
+	
+clearMPqueue:
+	MOVLW	0x00
+	MOVWF	mCountPri
+	MOVLW	0x5A
+	MOVWF	mBeginPri
+	RETURN
+clearCqueue:
+	MOVLW	0x00
+	MOVWF	cCount
+	MOVLW	0x66
+	MOVWF	cBegin
+	RETURN
+	
+clearCPqueue:
+	MOVLW	0x00
+	MOVWF	cCountPri
+	MOVLW	0x84
+	MOVWF	cBeginPri
 	RETURN
 	
 buttonInterrupt:
 				    ;Checks which button was pressed 
-	BTFSS	PORTB, RB7	     
+	BTFSC	PORTB, RB0	     
 	call	managerNormalButton	    ;RB0 pressed
-	BTFSS	PORTB, RB6	    
+	BTFSC	PORTB, RB1	    
 	call	managerPriorityButton	    ;RB1 pressed
-	BTFSS	PORTB, RB5	    
+	BTFSC	PORTB, RB2	    
     	call	cashierNormalButton	    ;RB2 pressed
-	BTFSS	PORTB, RB4	
+	BTFSC	PORTB, RB3	
 	call	cashierPriorityButton	    ;RB3 pressed
+	
+	RETURN
 
 ;(Next 4 functions) Buttons pressed. Functions called locally
 managerNormalButton:
-	;if counter == 0x43 -> reset counter
-;	MOVLW	0X19
-;	SUBWF	mCount, W
-;	BTFSS	STATUS, Z
-;	CALL	clearMqueue
-;	
+	
+	MOVLW	0x1E
+	SUBWF	mCount, W
+	BTFSC	STATUS, Z
+	CALL	clearMqueue
+	
 	INCF	mCount
 	MOVF	mBegin, W
 	MOVWF	FSR
 	MOVF	mCount, W
 	MOVWF	INDF
-	
 	RETURN	
+
 managerPriorityButton:
-	;TODO
+	MOVLW	0xC
+	SUBWF	mCountPri, W
+	BTFSC	STATUS, Z
+	CALL	clearMPqueue
+	
+	INCF	mCountPri
+	MOVF	mBeginPri, W
+	MOVWF	FSR
+	MOVF	mCountPri, W
+	MOVWF	INDF
 	RETURN
+	
 cashierNormalButton:
-	;TODO
+	MOVLW	0x1E
+	SUBWF	cCount, W
+	BTFSC	STATUS, Z
+	CALL	clearCqueue
+	
+	INCF	cCount
+	MOVF	cBegin, W
+	MOVWF	FSR
+	MOVF	cCount, W
+	MOVWF	INDF
 	RETURN
 cashierPriorityButton:
-	;TODO
+	MOVLW	0xC
+	SUBWF	cCountPri, W
+	BTFSC	STATUS, Z
+	CALL	clearCPqueue
+	
+	INCF	cCountPri
+	MOVF	cBeginPri, W
+	MOVWF	FSR
+	MOVF	cCountPri, W
+	MOVWF	INDF
 	RETURN
 
 ; Called externally (read I/O pin). 
@@ -92,29 +143,35 @@ setup:
 	BANKSEL PORTB
 	CLRF	PORTB
 	BANKSEL	TRISB
-	MOVLW	b'11110000'	    ;Set RB<3:0> as inputs, RB<7:4> as outputs 
+	MOVLW	b'00001111'	    ;Set RB<3:0> as inputs, RB<7:4> as outputs 
 	MOVWF	TRISB ;
 	
 	
+	
+	;MOVLW	d'0'
+	;MOVWF	iCount
+	
+	
 	BANKSEL	PORTA		    ;Interruption setup
-	MOVLW	d'0'
-	MOVWF	iCount
-	
-	
-
 	MOVLW	b'11001000'	    ;enable global interruptions, pheriperals and PORTA
 	MOVWF	INTCON
+	BANKSEL	IOCB
+	MOVLW	b'00001111'	    ;enable global interruptions, pheriperals and PORTA
+	MOVWF	IOCB
+	
+	
+	BANKSEL	PORTA		    ;Interruption setup
 	
 	MOVLW	0x2A
 	MOVWF	mBegin
 	
-	MOVLW	0x44
+	MOVLW	0x5A
 	MOVWF	cBegin
 	
-	MOVLW	0x4F
+	MOVLW	0x66
 	MOVWF	mBeginPri
 	
-	MOVLW	0x68
+	MOVLW	0x84
 	MOVWF	cBeginPri
 	
 	
